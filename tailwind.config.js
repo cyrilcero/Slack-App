25f71f2eaae0fe363babeaa3b164339dc7f74377
cyrilcero <cyril.cero@gmail.com>
2023-11-11 12:30:32 +0800

    feat: add initial files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      Aubergine: "#4A154B",
+      Horchata: "#F4EDE4",
+      Black: "#1D1C1D",
+      White: "#FFFFFF",
+      SlackBlue: "#36C5F0",
+      SlackGreen: "#2EB67D",
+      SlackYellow: "#ECB22E",
+      SlackRed: "#E01E5A",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
