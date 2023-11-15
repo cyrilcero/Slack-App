57a95bc0c1e2f6d99f80b643ba242e59c5473dde
cyrilcero <cyril.cero@gmail.com>
2023-11-15 21:01:21 +0800

    style: add max-width

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -8,7 +8,7 @@ import { NavBar } from "./components/NavBar";
 function MainPage() {
   return (
     <>
-      <div className="flex w-full h-screen bg-[#313338]">
+      <div className="flex w-full h-screen bg-[#313338] max-w-[1920px]">
         <NavBar />
         <div className="flex h-full w-full text-white overflow-y-auto">
           <Outlet />
