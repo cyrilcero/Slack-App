7d882e5cceffd3940d90c280a08f64f0ac5fde08
cyrilcero <cyril.cero@gmail.com>
2023-11-17 00:58:51 +0800

    feat: add collapsing feature to channels and messages

--- src/pages/MainPage/components/Dashboard.jsx
+++ src/pages/MainPage/components/Dashboard.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
-import { TbTriangleInvertedFilled } from "react-icons/tb";
-
 import { toastSuccess, toastError, getLocalStorage } from "../../../utils";
-import { GoPlus } from "react-icons/go";
+import { GoPlus, GoTriangleDown, GoTriangleRight } from "react-icons/go";
 
 function SideBar() {
   const [loading, setLoading] = useState(false);
   const [channelData, setChannelData] = useState("");
+  const [channelVisibility, setChannelVisibility] = useState(true);
+  const [directMessageVisibility, setDirectMessageVisibility] = useState(true);
 
   useEffect(() => {
-    async function loadData() {
+    async function loadChannelData() {
       setLoading(true);
       const header_data = getLocalStorage("headerData");
       try {
@@ -33,7 +33,7 @@ function SideBar() {
         toastError("error");
       }
     }
-    loadData();
+    loadChannelData();
   }, []);
 
   return (
@@ -41,26 +41,43 @@ function SideBar() {
       <div className="flex flex-col w-1/4 min-w-[250px] p-4 h-full bg-[#2b2d31]">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
-            {/* <TbTriangleInvertedFilled /> */}
+            <div onClick={() => setChannelVisibility(!channelVisibility)}>
+              {channelVisibility ? <GoTriangleDown /> : <GoTriangleRight />}
+            </div>
             <span>Channels</span>
           </div>
           <GoPlus />
         </div>
-        <div>
-          {!loading && (
-            <ul>
-              {(channelData.data || []).map((item, idx) => (
-                <li className="pl-2 py-1 hover:bg-slate-400 w-full" key={idx}>
-                  <span>{item.name}</span>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+        {channelVisibility && (
+          <div className="">
+            {!loading && (
+              <ul>
+                {(channelData.data || []).map((item, idx) => (
+                  <li
+                    className="pl-2 py-1 hover:bg-slate-400 roudned-md w-full"
+                    key={idx}
+                  >
+                    <span>{item.name}</span>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        )}
 
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
-            {/* <TbTriangleInvertedFilled /> */}
+            <div
+              onClick={() =>
+                setDirectMessageVisibility(!directMessageVisibility)
+              }
+            >
+              {directMessageVisibility ? (
+                <GoTriangleDown />
+              ) : (
+                <GoTriangleRight />
+              )}
+            </div>
             <span>Direct Messages</span>
           </div>
           <GoPlus />
@@ -79,10 +96,10 @@ function Dashboard() {
           THINGS TO DO
           <div className="text-2xl text-left font-normal">
             <li className="text-blue-500">LOGIN/SIGNUP THINGS</li>
-            <li className="line-through text-red-400">
+            <li className="line-through text-green-400">
               User is able to create his/her account with email and password
             </li>
-            <li className="line-through text-red-400">
+            <li className="line-through text-green-400">
               User is able to login his/her credentials
             </li>
             <li className="text-blue-500">SIDEBAR THINGS</li>
@@ -91,14 +108,16 @@ function Dashboard() {
               https://www.flowbite-react.com/docs/components/tooltip
             </li>
             <li className="text-blue-500">CHANNEL THINGS</li>
-            <li className="text-green-400">
+            <li className="line-through text-green-400">
               User is able to create new channel
             </li>
-            <li className="text-green-400">
+            <li className="line-through text-green-400">
               User is able to add users on a channel
             </li>
-            <li className="">User is able to SEE all channels</li>
-            <li className="text-green-400">
+            <li className="line-through text-green-400">
+              User is able to SEE all channels
+            </li>
+            <li className="line-through text-green-400">
               Process flow: Click "+" then redirect, 1 input:name 1
               select:members 1 btn create channel, toast success then reload
               sidebar to show new channels
