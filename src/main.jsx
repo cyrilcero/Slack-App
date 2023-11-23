f9d44578f74d16211d464b881f422879536c034b
cyrilcero <cyril.cero@gmail.com>
2023-11-24 02:14:52 +0800

    feat: add error page elements in routes

--- src/main.jsx
+++ src/main.jsx
@@ -12,24 +12,32 @@ import MainPage from "./pages/MainPage/MainPage";
 import Sidebar from "./pages/MainPage/components/Sidebar.jsx";
 import AllUsers from "./pages/MainPage/components/AllUsers";
 import CreateChannel from "./pages/MainPage/components/CreateChannel.jsx";
-import { ProtectedRoute } from "./utils";
+import { ProtectedRoute, LoggedInRoute } from "./utils";
 import MessageArea, {
   NoSelectedChat,
 } from "./pages/MainPage/components/MessageArea.jsx";
 import ChannelMessageArea from "./pages/MainPage/components/ChannelMessageArea.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <LoggedInRoute>
+        <App />
+      </LoggedInRoute>
+    ),
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <LoginPage />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "sign-up",
         element: <SignUpPage />,
+        errorElement: <ErrorPage />,
       },
     ],
   },
@@ -40,6 +48,7 @@ const routes = createBrowserRouter([
         <MainPage />
       </ProtectedRoute>
     ),
+    errorElement: <ErrorPage />,
     children: [
       {
         element: <Sidebar />,
@@ -47,24 +56,29 @@ const routes = createBrowserRouter([
           {
             index: true,
             element: <NoSelectedChat />,
+            errorElement: <ErrorPage />,
           },
           {
             path: "t/:id",
             element: <MessageArea />,
+            errorElement: <ErrorPage />,
           },
           {
             path: "c/:id",
             element: <ChannelMessageArea />,
+            errorElement: <ErrorPage />,
           },
         ],
       },
       {
         path: "create-channel",
         element: <CreateChannel />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "all-users",
         element: <AllUsers />,
+        errorElement: <ErrorPage />,
       },
     ],
   },
