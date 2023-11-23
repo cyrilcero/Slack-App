7d1696536543b6fd1ee44397a87f17402ef6ee77
cyrilcero <cyril.cero@gmail.com>
2023-11-24 02:14:26 +0800

    feat: add Error page

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,19 @@
+import { GoAlert } from "react-icons/go";
+
+function ErrorPage() {
+  return (
+    <section>
+      <div className="flex flex-col justify-center items-center w-full h-screen p-4 text-5xl">
+        <container className="flex flex-col justify-center items-center bg-[#313338] p-12 rounded-lg text-white">
+          <GoAlert className="text-9xl text-red-400" />
+          <span className="font-bold my-4">Something is wrong? 🤔</span>
+          <span className="text-base font-light italic text-gray-400">
+            Please blame the developer
+          </span>
+        </container>
+      </div>
+    </section>
+  );
+}
+
+export default ErrorPage;
