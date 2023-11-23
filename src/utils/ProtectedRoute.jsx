b5b7dba40c89b9b78c93b413fc6bad2a4c6e7a00
cyrilcero <cyril.cero@gmail.com>
2023-11-24 02:15:46 +0800

    feat: add protected routes for logged in check

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,11 +1,20 @@
 import { Navigate } from "react-router-dom";
-import { toastError } from "./toasts";
+import { toastError, toastInfo } from "./toasts";
 
 export function ProtectedRoute({ children }) {
   const headerData = JSON.parse(localStorage.getItem("headerData"));
   if (!headerData) {
     toastError("Please login");
     return <Navigate to="/" replace={true} />;
+  }
+  return <>{children}</>;
+}
+
+export function LoggedInRoute({ children }) {
+  const loginData = JSON.parse(localStorage.getItem("LoginData"));
+  if (loginData) {
+    toastInfo("You are already logged in");
+    return <Navigate to="/app" replace={true} />;
   }
   return <>{children}</>;
 }
