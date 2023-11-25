feaa319815ffeeccd2c06f661c6cf93160924dd2
cyrilcero <cyril.cero@gmail.com>
2023-11-25 23:21:06 +0800

    feat: add individual api call util

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -43,3 +43,48 @@ export function useFetch(url, config) {
 
   return { data, setData, response, error, loading, fetchAPI };
 }
+
+export function useIndividualFetch() {
+  const [data, setData] = useState(null);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function individualFetchAPI(url, config) {
+    setLoading(true);
+    const header_data = getLocalStorage("headerData");
+    const token = header_data?.["access-token"];
+    const client = header_data?.["client"];
+    const expiry = header_data?.["expiry"];
+    const uid = header_data?.["uid"];
+    const API_URL = "http://206.189.91.54/api/v1" + url;
+    try {
+      const response = await fetch(API_URL, {
+        method: config.method,
+        headers: {
+          ...config.header,
+          "Content-Type": "application/json",
+          "access-token": token,
+          client: client,
+          expiry: expiry,
+          uid: uid,
+        },
+        body: config.body ? JSON.stringify(config.body) : null,
+      });
+      if (response === 200) {
+        setResponse(response);
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setError(error);
+      toastError(`${error}`);
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { data, setData, response, error, loading, individualFetchAPI };
+}
